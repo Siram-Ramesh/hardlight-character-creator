924f7908f3b428962071c3e826f55e799a83f4b1
Sriram Ramesh <srirames403735@gmail.com>
2025-02-19 18:13:26 -0800

    Create rudimentary page for viewing characters

---
+++ src/components/characterViewer/CharacterViewerIndex.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+
+function CharacterViewerIndex() {
+
+    let [characters, setCharacters] = useState();
+
+    return(
+        <div>
+            <div className="character-recycler-view">
+                <ol>
+                    {/*TODO: create a character summary component */}
+                </ol>
+            </div>
+            <div>
+                <button>Create new character</button>
+                <button>Import a character</button>
+            </div>
+        </div>
+    )
+}
+
+export default CharacterViewerIndex;
