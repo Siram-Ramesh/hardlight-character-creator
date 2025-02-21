edb45880e9ce4ab9f5efd96f5b2f8a1e0f5cfc2f
Sriram Ramesh <srirames403735@gmail.com>
2025-02-20 20:20:29 -0800

    Add basic styling to the compendium index page

--- src/components/compendium/CompendiumIndex.tsx
+++ src/components/compendium/CompendiumIndex.tsx
@@ -3,22 +3,36 @@ import  {Link} from 'react-router';
 function CompendiumIndex() {
 
     return (
-        <div>
-            <button className = "search-all-traits-link">
-                <Link to="traits">Search Through All Traits</Link>
-            </button>
-            <button className = "compendium-category">
-                <Link to="talents">Talents</Link>
-            </button>
-            <button className = "compendium-category">
-                <Link to="gear">Gear</Link>
-            </button>
-            <button className = "compendium-category">
-                <Link to="backgrounds">Backgrounds</Link>
-            </button>
-            <button className = "compendium-category">
-                <Link to="species">Species</Link>
-            </button>
+        <div className='flex-col justify-around min-h-full pt-12 sm:pt-6 lg:pt-8'>
+            <h1 className='text-indigo-800 p-12 sm:p-6 lg:p-8'>Compendium</h1>
+
+            <div className='flex relative left-0 justify-items-start py-12 sm:py-6 lg:py-8'>
+                <button className = "p-12 sm:p-6 lg:p-8 bg-rose-900 hover:bg-rose-800 text-fuchsia-200">
+                    <Link to="traits" className='text-3xl'>Search Through All Traits</Link>
+                </button>
+            </div>
+
+            <div className='flex relative left-0 justify-items-start py-12 sm:py-6 lg:py-8'>
+                <button className = "p-12 sm:p-6 lg:p-8 bg-gray-500 text-gray-100">
+                    <Link to="talents" className='text-3xl'>Talents</Link>
+                    <p className='text-gray-300 text-xs sm:hidden'>Feature under construction</p>
+                </button>
+            </div>
+            <div className='flex relative left-0 justify-items-start py-12 sm:py-6 lg:py-8'>
+                <button className = "p-12 sm:p-6 lg:p-8 bg-gray-500 text-gray-100">
+                    <Link to="gear" className='text-3xl'>Gear</Link>
+                </button>
+            </div>
+            <div className='flex relative left-0 justify-items-start py-12 sm:py-6 lg:py-8'>
+                <button className = "p-12 sm:p-6 lg:p-8 bg-gray-500 text-gray-100">
+                    <Link to="backgrounds" className='text-3xl'>Backgrounds</Link>
+                </button>
+            </div>
+            <div className='flex relative left-0 justify-items-start py-12 sm:py-6 lg:py-8'>
+                <button className = "p-12 sm:p-6 lg:p-8 bg-gray-500 text-gray-100">
+                    <Link to="species" className='text-3xl'>Species</Link>
+                </button>
+            </div>
         </div>
       )
 }
--- src/components/compendium/CompendiumLayout.tsx
+++ src/components/compendium/CompendiumLayout.tsx
@@ -4,15 +4,13 @@ function CompendiumLayout() {
 
     return (
         <div>
-            <nav className = "navbar">
-                <ol>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/characters">Characters</Link>
-                    </li>
-                </ol>
+            <nav className = "flex top-0 relative mx-auto min-w-full min-h-12 px-12 sm:px-6 lg:px-8 bg-violet-950 justify-start">
+                <div className="px-12 lg:px-8 sm:px-6 hover:bg-violet-900 place-content-center">
+                    <Link to="/">Home</Link>
+                </div>
+                <div className="px-12 lg:px-8 sm:px-6 hover:bg-violet-900 place-content-center">
+                    <Link to="/characters">Characters</Link>
+                </div> 
             </nav>
             <Outlet/>
         </div>
