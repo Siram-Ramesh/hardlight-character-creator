d49ebf88932840acef0db235bbbef88771a82d34
Sriram Ramesh <srirames403735@gmail.com>
2025-02-21 22:33:09 -0800

    Update UI to use more responsive stylings

--- src/HomePageNavBar.tsx
+++ src/HomePageNavBar.tsx
@@ -3,15 +3,15 @@ import { Link } from "react-router"
 function HomePageNavBar() {
 
   return (
-    <nav className = "flex top-0 relative mx-auto w-screen px-12 sm:px-6 lg:px-8 bg-violet-950 justify-start">
+    <nav className = "flex top-0 relative mx-auto w-dvw px-12 sm:px-6 lg:px-8 bg-violet-950 sm:justify-center md:justify-start">
         <hgroup className = "px-12 lg:px-8 sm:px-6">
-            <h1>HARDLIGHT</h1>
-            <h4>Character Creator</h4>
+            <h1 className="sm:text-xl md:text-5xl">HARDLIGHT</h1>
+            <h4 className="sm:text-xl">Character Creator</h4>
         </hgroup>
-        <div className="px-12 lg:px-8 sm:px-6 hover:bg-violet-900 place-content-center">
+        <div className="sm:max-md:hidden px-12 lg:px-8 sm:px-6 hover:bg-violet-900 place-content-center">
             <Link to="/compendium">Compendium</Link>
         </div>
-        <div className="px-12 lg:px-8 sm:px-6 hover:bg-violet-900 place-content-center">
+        <div className="sm:max-md:hidden px-12 lg:px-8 sm:px-6 hover:bg-violet-900 place-content-center">
             <Link to="/characters">Characters</Link>
         </div>   
     </nav>
--- src/components/compendium/CompendiumIndex.tsx
+++ src/components/compendium/CompendiumIndex.tsx
@@ -4,33 +4,33 @@ function CompendiumIndex() {
 
     return (
         <div className='flex-col justify-around min-h-full pt-12 sm:pt-6 lg:pt-8'>
-            <h1 className='text-indigo-800 p-12 sm:p-6 lg:p-8'>Compendium</h1>
+            <h1 className='text-indigo-800 p-12 sm:p-6 lg:p-8 sm:text-4xl md:text-6xl'>Compendium</h1>
 
             <div className='flex relative left-0 justify-items-start py-12 sm:py-6 lg:py-8'>
                 <button className = "p-12 sm:p-6 lg:p-8 bg-rose-900 hover:bg-rose-800 text-fuchsia-200">
-                    <Link to="traits" className='text-3xl'>Search Through All Traits</Link>
+                    <Link to="traits" className='lg:text-3xl sm:text-lg'>Search Through All Traits</Link>
                 </button>
             </div>
 
             <div className='flex relative left-0 justify-items-start py-12 sm:py-6 lg:py-8'>
                 <button className = "p-12 sm:p-6 lg:p-8 bg-gray-500 text-gray-100">
-                    <Link to="talents" className='text-3xl'>Talents</Link>
+                    <Link to="talents" className='lg:text-3xl sm:text-lg'>Talents</Link>
                     <p className='text-gray-300 text-xs sm:hidden'>Feature under construction</p>
                 </button>
             </div>
             <div className='flex relative left-0 justify-items-start py-12 sm:py-6 lg:py-8'>
                 <button className = "p-12 sm:p-6 lg:p-8 bg-gray-500 text-gray-100">
-                    <Link to="gear" className='text-3xl'>Gear</Link>
+                    <Link to="gear" className='lg:text-3xl sm:text-lg'>Gear</Link>
                 </button>
             </div>
             <div className='flex relative left-0 justify-items-start py-12 sm:py-6 lg:py-8'>
                 <button className = "p-12 sm:p-6 lg:p-8 bg-gray-500 text-gray-100">
-                    <Link to="backgrounds" className='text-3xl'>Backgrounds</Link>
+                    <Link to="backgrounds" className='lg:text-3xl sm:text-lg'>Backgrounds</Link>
                 </button>
             </div>
             <div className='flex relative left-0 justify-items-start py-12 sm:py-6 lg:py-8'>
                 <button className = "p-12 sm:p-6 lg:p-8 bg-gray-500 text-gray-100">
-                    <Link to="species" className='text-3xl'>Species</Link>
+                    <Link to="species" className='lg:text-3xl sm:text-lg'>Species</Link>
                 </button>
             </div>
         </div>
