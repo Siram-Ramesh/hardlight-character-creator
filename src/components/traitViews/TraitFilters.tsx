6f9c77a2ba1a83211a1723faed6ff35d282c592a
Sriram Ramesh <srirames403735@gmail.com>
2025-02-21 22:35:30 -0800

    Add button to TraitViewer menu that pulls up an empty TraitFilters dialog

---
+++ src/components/traitViews/TraitFilters.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function TraitFilters() {
+
+    return(
+        <dialog className="flex min-h-max w-screen">
+
+        </dialog>
+    )
+}
+
+
+export default TraitFilters;
--- src/components/traitViews/TraitViewer.tsx
+++ src/components/traitViews/TraitViewer.tsx
@@ -1,25 +1,36 @@
 import { useState } from "react";
+import TraitFilters from "./TraitFilters";
+import { TraitFilter, TraitData } from "../types/TraitFilter";
 
 function TraitViewer() {
 
-    const [filters, setFilters] = useState(); //determines what TraitSummary components are populated into the recyclerView
-    const [selectedTrait, setSelectedTrait] = useState(); //determines what TraitDetails component is populated into the main div
+    const [filters, setFilters] = useState<TraitFilter>(null); //determines what TraitSummary components are populated into the recyclerView
+    const [showFilters, setShowFilters] = useState(false);
+    const [selectedTrait, setSelectedTrait] = useState<TraitData>(null); //determines what TraitDetails component is populated into the main div
 
-
-    return (
-        <div>
-            <div className="trait-filters">
-                {/*TODO: define a TraitFilters component*/}
+    // TODO: extract this to a new component
+    let filtersHiddenView = 
+        <div className="flex-col w-dvh min-h-max">
+            <div className="min-h-max w-dvw">
+                    <h1 className="float-left text-fuchsia-200 sm:text-xl md:text-5xl m-10 sm:m-4">Search All Traits</h1>
+                    <button onClick={() => setShowFilters(true)} className="float-right bg-rose-900 hover:bg-rose-800 text-fuchsia-200 sm:p-4 m-10 sm:m-4">
+                        Filter Traits...
+                    </button>
             </div>
-            <div>
+            <div className="flex min-h-max">
                 <div className="trait-recycler-view">
                     {/*TODO: define a TraitSummary component*/}
                 </div>
-                <div>
+                <div className="flex-col">
                     {/*TODO: define a TraitDetails component*/}
                 </div>
             </div>
         </div>
+
+    let filtersView = <TraitFilters/>
+
+    return (
+        showFilters ? filtersView : filtersHiddenView
     )
 }
 
